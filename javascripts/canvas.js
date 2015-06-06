3e1f5ceed0f0b1aa2d85d3e974b994fb4bb45ca7
Bridie <bridie.dibble@gmail.com>
2015-06-06 09:05:12 +0100

    Moving the events position logic out from the draw method

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -3,9 +3,7 @@ function Canvas() {
 	this.context = this.domElement.getContext('2d');
 }
 
-Canvas.prototype.draw = function(particle) {
-	var positionX = event.pageX - this.domElement.getBoundingClientRect().left - (particle.width / 2);
-	var positionY = event.pageY - this.domElement.getBoundingClientRect().top - (particle.height / 2);
+Canvas.prototype.draw = function(particle, positionX, positionY) {
 	this.context.fillStyle = particle.color;
-	this.context.fillRect(positionX, positionY, particle.width, particle.height);
+	this.context.fillRect(positionX - (particle.width / 2), positionY - (particle.height / 2), particle.width, particle.height);
 }
--- javascripts/index.js
+++ javascripts/index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var rock = new Rock();
 
 	canvas.domElement.addEventListener('click', function(event) {
-		canvas.draw(rock);
+		var positionX = event.pageX - canvas.domElement.getBoundingClientRect().left;
+		var positionY = event.pageY - canvas.domElement.getBoundingClientRect().top;
+		canvas.draw(rock, positionX, positionY);
 	});
 });
