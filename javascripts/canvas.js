0ab4333c4b83c41f70996b265f2e29901667130c
Bridie <bridie.dibble@gmail.com>
2015-06-06 08:47:44 +0100

    particle gets drawn in the correct place on the canvas

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,9 +1,11 @@
 function Canvas() {
-	this.id = document.getElementById('falling-sands-grid');
-	this.context = this.id.getContext('2d');
+	this.domElement = document.getElementById('falling-sands-grid');
+	this.context = this.domElement.getContext('2d');
 }
 
 Canvas.prototype.draw = function(particle) {
+	var positionX = event.pageX - this.domElement.getBoundingClientRect().left - (particle.width / 2);
+	var positionY = event.pageY - this.domElement.getBoundingClientRect().top - (particle.height / 2);
 	this.context.fillStyle = particle.color;
-	this.context.fillRect(event.pageX, event.pageY, particle.width, particle.height);
+	this.context.fillRect(positionX, positionY, particle.width, particle.height);
 }
--- javascripts/index.js
+++ javascripts/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var canvas = new Canvas();
 	var rock = new Rock();
 
-	canvas.id.addEventListener('click', function(event) {
+	canvas.domElement.addEventListener('click', function(event) {
 		canvas.draw(rock);
 	});
 });
