78ed2b1f96c8555bb001b3acee61eebdd97776b9
Bridie <bridie.dibble@gmail.com>
2015-06-09 19:12:41 +0100

    Rock gets drawn when mouse is clicked and moved. Drawing stops when the click is released or the mouse leaves the canvas.

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,9 +2,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var canvas = new Canvas();
 	var rock = new Rock();
 
-	canvas.domElement.addEventListener('click', function(event) {
+	var draw = function(event) {
 		var positionX = event.pageX - canvas.domElement.getBoundingClientRect().left;
 		var positionY = event.pageY - canvas.domElement.getBoundingClientRect().top;
 		canvas.draw(rock, positionX, positionY);
+	}
+
+	canvas.domElement.addEventListener('mousedown', function(event) {
+		canvas.domElement.addEventListener('mousemove', draw);
 	});
+
+	canvas.domElement.addEventListener('mouseup', function() {
+		canvas.domElement.removeEventListener('mousemove', draw);
+	});
+
+	canvas.domElement.addEventListener('mouseleave', function() {
+		canvas.domElement.removeEventListener('mousemove', draw);
+	});
+
 });
